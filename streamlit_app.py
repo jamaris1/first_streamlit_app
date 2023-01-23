9fda8be6554dab052a7cbb19fe0ae43d97f15154
jamaris1 <81471205+jamaris1@users.noreply.github.com>
2023-01-23 14:48:57 -0500

    Update streamlit_app.py
    Removing the json formatted response.

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ streamlit.dataframe(fruits_to_show)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 streamlit.header("Fruityvice Fruit Advice!")
-streamlit.text(fruityvice_response.json())
+#streamlit.text(fruityvice_response.json())
 
 # Normalizing the JSON format for better display 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
