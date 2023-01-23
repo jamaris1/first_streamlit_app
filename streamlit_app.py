03aea0abbd9303d2b296185ef3ce5f2ceb6e62c7
jamaris1 <81471205+jamaris1@users.noreply.github.com>
2023-01-23 14:30:49 -0500

    Update streamlit_app.py
    Adding the requests library
    Adding fruityvice_response

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 
 import streamlit
 import pandas
+import requests
+
 
 
 
@@ -24,6 +26,12 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
+
+
+streamlit.header("Fruityvice Fruit Advice!")
+streamlit.text(fruityvice_response)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 
 
