4fa07d848af3ea8602cc05f00f5fdffabf32a176
jamaris1 <81471205+jamaris1@users.noreply.github.com>
2023-01-24 07:52:05 -0500

    Update streamlit_app.py
    Remove stop

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,7 +69,7 @@ if streamlit.button('Get Fruit Load List'):
 #my_cur = my_cnx.cursor()
 #my_cur.execute("select * from fruit_load_list")
 
-streamlit.stop()
+#streamlit.stop()
 
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
