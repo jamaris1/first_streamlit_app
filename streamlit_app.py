e9790deb8a3d108c0b665d0bd31b283bafbc07a8
jamaris1 <81471205+jamaris1@users.noreply.github.com>
2023-01-24 07:27:44 -0500

    Update streamlit_app.py
    Correction type inside error handiling

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ try:
   #streamlit.text(fruityvice_response.json())
   fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
   if not fruit_choice:
-    steramlit.error("Please select a fruit to get information")
+    streamlit.error("Please select a fruit to get information")
   else:
     streamlit.write('The user entered ', fruit_choice)
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
