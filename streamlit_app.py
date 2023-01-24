710600bc568c1aa8619992164000433c0aa04322
jamaris1 <81471205+jamaris1@users.noreply.github.com>
2023-01-24 07:06:11 -0500

    Update streamlit_app.py
    Inserting data into fruit_load_list but this will give error.

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,6 +53,9 @@ streamlit.dataframe(my_data_rows)
 add_my_fruit = streamlit.text_input('What fruit would you like to add ?','Kiwi')
 streamlit.write('The user entered ', add_my_fruit)
 
+my_cur.execute("insert into pc_rivery_db.fruit_load_list values ('from streamlit');")
+
+
 
 
 
