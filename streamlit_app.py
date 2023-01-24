24f15017c291178cf5e25d915d8969b94a564dc9
jamaris1 <81471205+jamaris1@users.noreply.github.com>
2023-01-23 22:38:47 -0500

    Update streamlit_app.py
    Corrected a typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 #my_cur.execute("select * from fruit_load_list")
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_rows = my_cur.ferchall()
+my_data_rows = my_cur.fetchall()
 streamlit.header("Hello from Snowflake:")
 streamlit.dataframe(my_data_rows)
 
